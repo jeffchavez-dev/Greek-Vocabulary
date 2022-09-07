91d67b3df62a9d8bb366b61694df76929375e8c1
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-09-07 13:57:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,37 @@ const vocabulary = [
     { 
         "ἀγάπη": "love",
         "γῆ ἡ": "earth, land, ground (geology)",
-        "ζωή ἡ": "life (zoo, zoology)"
+        "ζωή ἡ": "life (zoo, zoology)",
+        "φωνή, ἡ ": "voice sound (phonetics, phonograph, phone)"
+
     } ,
     { 
-        "φωνή, ἡ voice": "sound (phonetics, phonograph, phone)",
+       
         "γῆ ἡ": "earth, land, ground (geology)",
         "ζωή ἡ": "life (zoo, zoology)"
     }              
 ]
 
-console.log(vocabulary[0]["ἀγάπη"])
 
+const vocabKeys = Object.keys(vocabulary[0])
+console.log(vocabKeys[3])
+
+let currentVocab = 0;
 //load vocabulary 
 
 const greekVocab = document.querySelector(".greek-vocabulary")
 const englishVocab = document.querySelector(".english-vocabulary")
+const correct = document.querySelector(".correct-answer")
 
 const loadVocabulary = () => {
-    greekVocab.innerHTML = vocabulary[0]["ἀγάπη"];
+    greekVocab.innerHTML = vocabKeys[currentVocab];
+    englishVocab.innerHTML = vocabulary[0][vocabKeys[currentVocab]]
 }
+
+correct.addEventListener("click", ()  => {
+    currentVocab++;
+    loadVocabulary()
+    console.log(currentVocab)
+})
 
 loadVocabulary()
