4e9b29eac808f598166cbaed12e8d57294885527
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-09-07 13:21:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const vocabulary = {
-    lesson1: {
-        "ἀγάπη ἡ": "love",
-        "γῆ ἡ": "earth, land, ground (geology)",
-        "ζωή ἡ": "life (zoo, zoology)",
-    }, 
-    lesson1: {
-        "ἀγάπη ἡ": "love",
-        "γῆ ἡ": "earth, land, ground (geology)",
-        "ζωή ἡ": "life (zoo, zoology)",
-    }, 
+const vocabulary = [
+    { 
+        "lesson1": {
+            "ἀγάπη ἡ": "love",
+            "γῆ ἡ": "earth, land, ground (geology)",
+            "ζωή ἡ": "life (zoo, zoology)"
+        },
+        "lesson1": {
+            "ἀγάπη ἡ": "love",
+            "γῆ ἡ": "earth, land, ground (geology)",
+            "ζωή ἡ": "life (zoo, zoology)"
+        }   
+    }
+]
+
+console.log(vocabulary.lesson1)
+
+//load vocabulary 
+
+const greekVocab = document.querySelector(".greek-vocabulary")
+const englishVocab = document.querySelector(".english-vocabulary")
+
+const loadVocabulary = () => {
+    greekVocab.innerHTML = vocabulary[0];
 }
 
-console.log(vocabulary[0])
+loadVocabulary()
