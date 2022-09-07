5530d318a6ee8fed6e4babaf4f38c54a3b1f0c85
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-09-07 13:01:36 +0800

    Vocabulary object

---
+++ script.js
@@ -0,0 +1,14 @@
+const vocabulary = {
+    lesson1: {
+        "ἀγάπη ἡ": "love",
+        "γῆ ἡ": "earth, land, ground (geology)",
+        "ζωή ἡ": "life (zoo, zoology)",
+    }, 
+    lesson1: {
+        "ἀγάπη ἡ": "love",
+        "γῆ ἡ": "earth, land, ground (geology)",
+        "ζωή ἡ": "life (zoo, zoology)",
+    }, 
+}
+
+console.log(vocabulary[0])
